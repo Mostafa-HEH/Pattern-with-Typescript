39c9cfa4a411f64b77daf3688658807aa8757ac4
Mostafa Hassan <Mostafa Hassan>
2021-02-14 08:27:06 +0200

    test as user

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123207,7 +123207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1808" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2658" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
